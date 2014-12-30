6b76d86b911940b12f46de9c96da5605ede74a4d
Dave Sierra <dave@hakusports.com>
2014-12-29 22:39:06 -0500

    Revolving background, footer credits

--- js/scripts.js
+++ js/scripts.js
@@ -14,6 +14,41 @@ $(window).load(function() {
 $(document).ready(function(){
 	console.log("First");
 
+	$.ajax({
+		url: 'theme.json',
+		dataType: 'json',
+		//async: false,
+		//cache: false,
+		success: function( data, status ){
+			console.log('working');
+			console.log( data.length + ' results found!' );
+			loadTheme(data);
+		},
+		error: function(xhr, textStatus, err) {
+			console.log('not working');
+			console.log("readyState: "+xhr.readyState+"\n xhrStatus: "+xhr.status);//4, 200 ok
+			console.log("responseText: "+xhr.responseText);//HERE IS CORRECT JSON CONTENT
+		}
+	});
+
+	function loadTheme(data) {
+
+
+		var siteThemes = data,
+			themeCount = siteThemes.length,
+			randomNumber = Math.floor( (Math.random() * themeCount) );
+		
+		chosenTheme = siteThemes[randomNumber];
+
+		console.log("Pick image: ", randomNumber, chosenTheme.image);
+		//BACKSTRETCH
+
+		$.backstretch(chosenTheme.image);
+		$(".wrapper").addClass(chosenTheme.color);
+		console.log('atest for watch')
+		$(".credit .tooltip").attr("href", chosenTheme.credit.link);
+		$(".credit .tooltip").tooltipster('content', chosenTheme.credit.name);
+	}
 
 
 	//MASONRY
